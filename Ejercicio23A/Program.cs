a1f436507d66782ffa88edcf57b341092e7e41cf
EricRequena <eric.requena.7e7@itb.cat>
2023-12-13 12:14:08 +0100

    Calc Funcion Rectangle closes #1

--- Ejercicio23A/Program.cs
+++ Ejercicio23A/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Ejercicio23A
+﻿using System;
+namespace Ejercicio23A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
            
+        }
+        public static int Rectangle(int OneVal, int SecondVal)
+        {
+            int resultat;
+
+            resultat = OneVal * SecondVal;
+            return resultat;
         }
     }
 }
