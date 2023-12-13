4d2ede899998b6a649e8ee42cb9e03dcf8b24407
EricRequena <eric.requena.7e7@itb.cat>
2023-12-13 11:59:20 +0100

    Add project files.

---
+++ Ejercicio23A/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio23A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
